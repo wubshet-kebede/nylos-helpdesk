6ac26674067906a67155c992cdc9e00f34562fd2
Wubshet <wubshet2829@gmail.com>
2026-08-16 19:13:18 +0300

    feat(tickets): register Tickets module database

--- backend/src/Modules/Tickets/Nylos.Helpdesk.Modules.Tickets/TicketsModule.cs
+++ backend/src/Modules/Tickets/Nylos.Helpdesk.Modules.Tickets/TicketsModule.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nylos.Helpdesk.Modules.Tickets.Infrastructure;
 
 namespace Nylos.Helpdesk.Modules.Tickets;
 
@@ -8,7 +10,9 @@ public static class TicketsModule
     public static IServiceCollection AddTicketsModule(
         this IServiceCollection services, 
         IConfiguration configuration)
-    {
+    {  
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<TicketsDbContext>(options => options.UseNpgsql(connectionString, npgsql => npgsql.MigrationsHistoryTable("__EFMigrationsHistory", "tickets")));
         return services;
     }
 }
